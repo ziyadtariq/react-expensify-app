29e8e719ab3ebe19a74f17a9aa85562f05957317
ziyadtariq <ziyad.tariq76@gmail.com>
2018-05-02 13:55:44 -0500

    Set to delete

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Ziyad = "delete this line";
 
 module.exports = (env) => {
   const isProduction = env === 'production';
