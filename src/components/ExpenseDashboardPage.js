3daf656fd888adaf4fb7394c7992d844a88601e6
ziyadtariq <ziyad.tariq76@gmail.com>
2018-05-16 11:04:26 -0500

    add private routes

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 import ExpensesSummary from './ExpensesSummary';
+import Header from '../components/Header';
 
 
 const ExpenseDashboardPage = () => (
