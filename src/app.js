58f4373f2e7315b0cae6a45f932c3f253e8ae166
ziyadtariq <ziyad.tariq76@gmail.com>
2018-05-19 15:59:09 -0500

    Add Expensify Styles

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LodingPage from './components/LodingPage';
 
 const store = configureStore();
 
@@ -26,7 +27,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LodingPage/>, document.getElementById('app'));
 
 
 
